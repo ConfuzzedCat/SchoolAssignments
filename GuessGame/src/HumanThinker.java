5bf611fe079ef78b6079c07b6bdde65400336f51
ConfuzzedCat <73724128+ConfuzzedCat@users.noreply.github.com>
2022-10-31 11:42:43 +0100

    updated to latest version

--- GuessGame/src/HumanThinker.java
+++ GuessGame/src/HumanThinker.java
@@ -1,53 +1,5 @@
-import java.util.Scanner;
-
-public class HumanThinker
+public class HumanThinker extends AHumanPlayer implements IThinker
 {
-	private static final Scanner scanner = new Scanner(System.in);
-	private String name;
-
-	public HumanThinker()
-	{
-		name = null;
-	}
-
-	public void yourTurn()
-	{
-		//Clear the console
-		for(int i = 0; i < 100; ++i)
-		{
-			System.out.println();
-		}
-		System.out.println(getName() + " it is now your turn.");
-		System.out.println("Press ENTER when ready!");
-		scanner.nextLine();
-		//Clear the console
-		for(int i = 0; i < 100; ++i)
-		{
-			System.out.println();
-		}
-	}
-
-	public String getName()
-	{
-		if(name != null)
-		{
-			return name;
-		}
-
-		System.out.print("Thinker, please enter your name: ");
-		name = scanner.nextLine();
-		return name;
-	}
-
-	public void newGame(int min, int max, String opponent)
-	{
-		System.out.println("A new game is about to start. You are the thinker.");
-		System.out.println("You are plaing against " + opponent + ".");
-		System.out.println("The range is from " + min + " to " + max + ".");
-		System.out.println("Please think of a number and press ENTER when ready.");
-		scanner.nextLine();
-	}
-
 	public Answer evaluateGuess(int guess, String opponent)
 	{
 		System.out.println(opponent + " guessed: " + guess);
@@ -74,10 +26,11 @@ public class HumanThinker
 		}
 	}
 
-	public void endOfGame(int numberOfGuesses, String opponent)
+	public boolean endOfGame(int numberOfGuesses, String opponent)
 	{
 		System.out.println("Game over.");
-		System.out.println(opponent + " used " + numberOfGuesses + " to guess your secret number.");
+		String guesses = numberOfGuesses == 1 ? "guess" : "guesses";
+		System.out.println(opponent + " used " + numberOfGuesses + " " + guesses + " to guess your secret number");
+		return playAgain();
 	}
-
 }
